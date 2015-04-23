5e1f86365f86b66b9dd4e5876152b54734cefdbc
Malarkey73 <s.henderson@ucl.ac.uk>
2015-04-23 12:28:33 +0100

    a more or less working version of read.mutations

--- R/read.mutations.R
+++ R/read.mutations.R
@@ -1,5 +1,5 @@
 #' Reads data from a MAF or VCF type file.
-#' This is a wrapper for read.delim that reads mutation data from a tab delimited text file such as a MAF or VCF file.
+#' This is a wrapper for read.delim that reads mutation data from a tab delimited text file such as a MAF, VCF or other tab-delimited tsv file.
 #' The input file must have at least 5 columns specifying in order 1:5 chromosome, start position, end position, reference sequence, and the mutant sequence. 
 #' For these any header names will be ignored and the imported data.frame will have the columns chr, start, end, ref, and mut.
 #' If there are additonal columns specifying strand or sampleID these can be imported too either by name (if there is  a header) or number.
@@ -12,57 +12,53 @@
 #' @param skip VCF or MAF files often have a few rows of comments that you will want to skip over (numeric, default=0).
 #' @keywords rainfall, mutants, kataegesis
 #' @examples
-#' # A single example data file comes with the package
-#' (exF <- system.file("extdata", "1317049.tsv", package="kat"))  
+#' # A single example data file with multiple tumour samples comes with the package
+#' (exF <- system.file("extdata", "1317049.tsv.gz", package="kater"))  
 #' # a minimal example
-#' PD4107a_min <- read.mutations(file=exF, sampleID = Sample.Name)
+#' PD4107a <- read.mutations(file=gzfile(exF), sampleID = Sample.Name)
 
-read.mutations <- function(file,  strand=strand, sampleID = sampleID, skip=0, header=T, ...){
+read.mutations <- function(file,  strand=strand, sampleID = sampleID, pos=1:5, skip=0, header=T, ...){
   
   # Non Standard Evaluation: see http://adv-r.had.co.nz/Computing-on-the-language.html#capturing-expressions
-  strand=deparse(substitute(strand))
-  sampleID = deparse(substitute(sampleID))
-  temp = read.delim(file, as.is=T, ...)
+  strand <- deparse(substitute(strand))
+  sampleID <- deparse(substitute(sampleID))
+  tempDF <- read.delim(file, as.is=T, ...)
+ 
   
+
   # check that at least the 5 main columns exist - fail if not.
-  if(ncol(temp) > 5 && nrow(temp) > 1)
+  if(ncol(tempDF) >= 5 && nrow(tempDF) > 1)
   {
-    TSV
-    VCF <- data.frame(chr= temp[,chr], start.position= temp[,start.position], end.position= temp[,end.position])
+    # give standard header names
+    DF = tempDF[, pos]
+    colnames(DF)[1:5] <- c("chr", "start", "end", "ref", "mut")
   }
   else
   {
-    stop(" A valid VCF needs at least chr, start.position, and end.position columns - check your file has these correctly named columns")
+    stop("The data has insufficient columns or was somehow corrupted on import. Check the data format and function options")
   }
   
-  # using the dplyr tbl throughout this package
-  library(dplyr)
-  VCF=as.tbl(VCF)
+  # if strand or sampleID columns matchin the data were specified then add them
+  if(any(colnames(tempDF) == strand))
+    DF$strand <- tempDF[,strand]
+  if(any(colnames(tempDF) == sampleID))
+    DF$sampleID <- tempDF[, sampleID]
   
-  # add these columns if they exist
-  if(strand %in% colnames(temp))
-    VCF <- mutate(VCF, strand = temp[,strand])
-  if(WT %in% colnames(temp))
-    VCF <- mutate(VCF, WT = temp[,WT])
-  if(MUT %in% colnames(temp))
-    VCF <- mutate(VCF, MUT = temp[,MUT])
-  if(sampleID %in% colnames(temp))
-    VCF <- mutate(VCF, sampleID = temp[,sampleID])
-  
-  # if you want the other columns in the VCF then set "other = TRUE"
-  if(other==TRUE)
-  {
-    other.cols<- setdiff(colnames(temp), c(chr,start.position,end.position, strand, WT, MUT, sampleID))
-    VCF <- as.tbl(cbind(VCF, temp[,other]))
-  }
+  # check the data looks of the correct type
+  if(!is.integer(DF$start) | !is.integer(DF$end))
+    stop("The start or end position data is not of type integer. Check the data format and function options")
+  if(!is.integer(DF$start) | !is.integer(DF$end))
+    stop("The start or end position data is not of type integer. Check the data format and function options")
+  if(!(any(DF$chr==1) | any(tolower(DF$chr) == "chr1")))
+     stop("The chromosome column looks odd, no 1's or chr1's. Check the data format and function options")
   
   
-  #short summary message and return a chr/pos sorted dplyr style tbl/data.frame
-  cat(c("VCF contains: ", paste0(colnames(VCF), ", "), "\n\n"))
-  VCF <- VCF %>%
-    arrange(chr, start.position)
-  
-  class(VCF)<- c(class(VCF), "VCF")
-  return(VCF)
+  # short message and return data as a dplyr style data.frame
+  library(magrittr)
+  DF=dplyr::as.tbl(DF)
+  cat(c("imported mutations data.frame contains: ", paste0(colnames(DF), ", "), "\n\n"))
+  DF <- DF %>%
+    dplyr::arrange(chr, start)
+  return(DF)
   
 }
