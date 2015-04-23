e590d05eb49904b71004b9fb2fb553b30202926a
Malarkey73 <s.henderson@ucl.ac.uk>
2015-04-23 16:36:43 +0100

    added working pointmuts o count the point mutations e.g. C>A or T>C etc

--- R/expandMutations.R
+++ R/expandMutations.R
@@ -10,24 +10,30 @@
 #' # This will add a column of the triplet context surrounding point mutations
 #' expandseq(DF, expand=1)
 
-expandMutations<- function(DF, expand=1, genomeseq = BSgenome.Hsapiens.UCSC.hg19)
+expandMutations<- function(DF, expand=1, genome = BSgenome.Hsapiens.UCSC.hg19)
 {
 
   #NSE
-  genomeseq=deparse(substitute(genomeseq))
+  genome=deparse(substitute(genome))
   
+  if(grep("UCSC", genome))
+    {
+    # This is the format of the UCSC.hg19 data "chr19" NOT "19", "chrX" not "chr23"
+      DF <- DF %>%
+        dplyr::mutate(adj.chr = ifelse(chr %in% 1:24, yes=paste0("chr", chr), no=chr)) %>%
+        dplyr::mutate(adj.chr = ifelse(adj.chr=="chr23", yes="chrX", no=adj.chr))
+    }
+    else
+    {
+      DF <- DF %>%
+        dplyr::mutate(adj.chr = chr)
+    }
   
-  # This is the format of the UCSC.hg19 data "chr19" NOT "19", "chrX" not "chr23"
-  DF <- DF %>%
-    dplyr::mutate(adj.chr = ifelse(chr %in% 1:24, yes=paste0("chr", chr), no=chr)) %>%
-    dplyr::mutate(adj.chr = ifelse(adj.chr=="chr23", yes="chrX", no=adj.chr))
-  
-  # load the right library
-  genome <- BSgenome::getBSgenome(genomeseq)
+  # select the right library
+  genome <- BSgenome::getBSgenome(genome)
   message("This may take a few seconds.")
   # if that library is human then use this to access the sequence data.
-  if(grep("Hsapiens", genomeseq))
-    gs <- BSgenome::getSeq(genome, DF$adj.chr, start= DF$start - 1, end= DF$end + 1)
+  gs <- BSgenome::getSeq(genome, DF$adj.chr, start= DF$start - 1, end= DF$end + 1)
   
   DF<- DF %>% 
     dplyr::mutate(expandref = as.character(gs)) %>%
